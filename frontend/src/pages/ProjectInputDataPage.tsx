9e65ed2a021b4f235c2bdf3ad9c5d11eb0c1c9fa
tiongg <tantiongguan123@gmail.com>
2025-03-12 18:01:16 +0800

    chore: add dummy data

--- frontend/src/pages/ProjectInputDataPage.tsx
+++ frontend/src/pages/ProjectInputDataPage.tsx
@@ -45,21 +45,21 @@ export default function ProjectInputDataPage() {
   const canTranscribe = formData.patientId && formData.transcription;
 
   return (
-    <main className='min-h-screen flex flex-col mx-8 my-auto gap-2'>
-      <div className='flex justify-between items-center my-8 text-blue-700'>
-        <button className='flex-1' onClick={() => navi('/home')}>
+    <main className="min-h-screen flex flex-col mx-8 my-auto gap-2">
+      <div className="flex justify-between items-center my-8 text-blue-700">
+        <button className="flex-1" onClick={() => navi(-1)}>
           <FaChevronLeft />
         </button>
-        <p className='font-bold text-2xl m-auto'>Input</p>
-        <div className='flex-1' />
+        <p className="font-bold text-2xl m-auto">Input</p>
+        <div className="flex-1" />
       </div>
 
-      <div className='flex flex-col gap-4'>
+      <div className="flex flex-col gap-4">
         <div>
           <p>Patient ID</p>
           <input
-            type='text'
-            className='border border-gray-300 rounded-md w-full p-2'
+            type="text"
+            className="border border-gray-300 rounded-md w-full p-2"
             value={formData.patientId}
             onChange={e =>
               updateFormData(draft => {
@@ -72,7 +72,7 @@ export default function ProjectInputDataPage() {
         <div>
           <p>Visit</p>
           <select
-            className='border border-gray-300 rounded-md w-full p-2'
+            className="border border-gray-300 rounded-md w-full p-2"
             value={formData.visit}
             onChange={e =>
               updateFormData(draft => {
@@ -88,7 +88,7 @@ export default function ProjectInputDataPage() {
           </select>
         </div>
 
-        <div className='flex flex-col gap-1'>
+        <div className="flex flex-col gap-1">
           <VoiceMemoRecorder
             onRecordedText={transcription =>
               updateFormData(draft => {
@@ -99,7 +99,7 @@ export default function ProjectInputDataPage() {
 
           <p>Transcribed words:</p>
           <textarea
-            className='border border-gray-300 rounded-md w-full p-2'
+            className="border border-gray-300 rounded-md w-full p-2"
             rows={6}
             onChange={e => {
               updateFormData(draft => {
@@ -128,33 +128,33 @@ export default function ProjectInputDataPage() {
       <Modal
         isOpen={isModalOpen}
         onClosed={() => setModalOpen(false)}
-        className='bg-white p-4 w-[375px] h-[70%] rounded-md'
+        className="bg-white p-4 w-[375px] h-[70%] rounded-md"
       >
-        <div className='flex flex-col gap-4 h-full'>
-          <div className='self-center p-2'>
-            <p className='text-blue-500 text-2xl font-bold'>
+        <div className="flex flex-col gap-4 h-full">
+          <div className="self-center p-2">
+            <p className="text-blue-500 text-2xl font-bold">
               Transcription complete!
             </p>
           </div>
 
           <div>
-            <p className='text-2xl font-bold'>
+            <p className="text-2xl font-bold">
               Patient ID: {formData.patientId}
             </p>
-            <p className='text-xl font-bold'>
+            <p className="text-xl font-bold">
               Visit: {getVisitLabel(formData.visit)}
             </p>
             {Object.entries(dataMappings?.data ?? {}).map(([key, value]) => (
-              <div key={key} className='flex gap-2'>
-                <p className='font-semibold'>{key}:</p>
+              <div key={key} className="flex gap-2">
+                <p className="font-semibold">{key}:</p>
                 <p>{value as any}</p>
               </div>
             ))}
           </div>
 
-          <div className='flex gap-4 mt-auto'>
+          <div className="flex gap-4 mt-auto">
             <button
-              className='rounded-full bg-red-500 text-white py-2 px-4 flex-1'
+              className="rounded-full bg-red-500 text-white py-2 px-4 flex-1"
               onClick={() => {
                 setModalOpen(false);
               }}
@@ -162,7 +162,7 @@ export default function ProjectInputDataPage() {
               Retranscribe
             </button>
             <button
-              className='rounded-full bg-blue-500 text-white py-2 px-4 flex-1'
+              className="rounded-full bg-blue-500 text-white py-2 px-4 flex-1"
               onClick={async () => {
                 const { data: res } = await axiosInstance.post(
                   '/excel/generate',
--- frontend/src/pages/ProjectPage.tsx
+++ frontend/src/pages/ProjectPage.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import dayjs from 'dayjs';
-import { FaChevronLeft } from 'react-icons/fa';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { HiDownload } from 'react-icons/hi';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { axiosInstance } from '../utils/axios-instance.ts';
 import { downloadFile } from '../utils/download-file.util.ts';
@@ -36,43 +37,53 @@ export default function ProjectPage() {
   } = projectDetails;
 
   return (
-    <main className='min-h-screen flex flex-col mx-8 my-auto gap-2'>
-      <div className='flex justify-between items-center my-8 text-blue-700'>
-        <button className='flex-1' onClick={() => navi('/home')}>
+    <main className="min-h-screen flex flex-col mx-8 my-auto gap-2">
+      <div className="flex justify-between items-center my-8 text-blue-700">
+        <button className="flex-1" onClick={() => navi('/home')}>
           <FaChevronLeft />
         </button>
-        <p className='font-bold text-2xl m-auto'>Details</p>
-        <div className='flex-1' />
+        <p className="font-bold text-2xl m-auto">Details</p>
+        <div className="flex-1" />
       </div>
 
-      <div>
-        <p className='text-gray-700 font-medium'>Project Name:</p>
-        <p>{projectName}</p>
+      <div className="flex flex-col gap-2">
+        <div>
+          <p className="text-gray-700 font-medium">Project Name:</p>
+          <p>{projectName}</p>
+        </div>
 
-        <p className='text-gray-700 font-medium'>Description:</p>
-        <p>{projectDescription || 'No description provided'}</p>
+        <div>
+          <p className="text-gray-700 font-medium">Description:</p>
+          <p>{projectDescription || 'No description provided'}</p>
+        </div>
 
-        <p className='text-gray-700 font-medium'>Created on:</p>
-        <p>{dayjs(createdAt).format('DD MMM YYYY')}</p>
+        <div>
+          <p className="text-gray-700 font-medium">Created on:</p>
+          <p>{dayjs(createdAt).format('DD MMM YYYY')}</p>
+        </div>
 
-        <p className='text-gray-700 font-medium'>Last updated:</p>
-        <p>{dayjs(lastUpdated).fromNow()}</p>
+        <div>
+          <p className="text-gray-700 font-medium">Last updated:</p>
+          <p>{dayjs(lastUpdated).fromNow()}</p>
+        </div>
 
         <button
-          className='bg-blue-500 text-white rounded-full py-2 px-4'
+          className="bg-blue-500 rounded-full text-white py-2 px-4 flex flex-row gap-2"
           onClick={() => {
             downloadFile(downloadUrl, `${projectName}.xlsx`);
           }}
         >
-          Download Spreadsheet
+          <HiDownload className="my-auto" />
+          <p className="font-semibold">Download Spreadsheet</p>
         </button>
       </div>
 
       <Link
         to={`/project/${id}/insert-data`}
-        className='bg-blue-500 text-white rounded-full py-2 px-4'
+        className="bg-blue-500 text-white rounded-full py-2 px-4 flex flex-row gap-2 justify-between"
       >
-        Insert Data
+        <p>Insert Data</p>
+        <FaChevronRight className="my-auto" />
       </Link>
     </main>
   );
